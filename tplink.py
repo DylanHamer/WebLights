f84b1ed6fefc92039e5035d4a989ff31b51fe456
Dylan Hamer <dylanhamer13@gmail.com>
2018-04-23 12:30:42 +0000

    Update tplink.py

--- tplink.py
+++ tplink.py
@@ -7,6 +7,11 @@ By Dylan Hamer
 from pyHS100 import SmartPlug, SmartBulb, Discover
 
 class tplink:
-    def __init__
+    def __init__(self):
+        pass
+    
+    def getDevices(self):
+        for device in Discover.discover().values():
+    
     
 
